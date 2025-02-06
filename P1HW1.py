ce11e928df6dba5e2e4fef25883b0ea3eaef165e
mathiasa9952 <mathiasa9952@student.faytechcc.edu>
2025-02-06 11:47:51 -0500

    Update P1HW1.py

--- P1HW1.py
+++ P1HW1.py
@@ -3,3 +3,21 @@
 # P1HW1
 # Collecting user information
 
+#get user info for integer numbers
+
+base_value = int(input("Enter an integer as the base value: " ))
+exponent = int(input("Enter an integer as the exponent: " )) 
+print()
+result = base_value ** exponent
+
+print (base_value, "raised to the power of", exponent, "is", result, "!!")
+print()
+print("-" *5, "Addition and Substarction", "-" *4)
+print()
+starting_integer = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: " ))
+subtract = int(input("Enter an integer to subtract: " ))
+result = starting_integer + add - subtract
+print()
+print(starting_integer, "+", add, "-", subtract, "is equal to", result)
+print()
