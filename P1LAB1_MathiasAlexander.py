ec4d3e2e6dd70cd30f3e8a3d442f50cf4191e897
mathiasa9952 <mathiasa9952@student.faytechcc.edu>
2025-02-06 10:16:48 -0500

    Add files via upload
    First upload

---
+++ P1LAB1_MathiasAlexander.py
@@ -0,0 +1,13 @@
+# Alexander Mathias
+# 02/04/2025
+# P1LAB1
+# Using the input and print functions to display a name
+
+# get user input for first name
+
+first_name = input ("Enter your first name: ")
+last_name = input ("Enter your last name: ")
+
+# Display welcome message using first and last name
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110!") 
